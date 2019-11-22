b39177c1f1d09ac2c9c174f0018e34232d20958b
benjamin.ward <benjamin.ward@daxko.com>
2019-11-22 00:03:01 -0600

    Update add event function to read firestore, handle refresh tokens.

--- functions/src/addEventToCalendar.js
+++ functions/src/addEventToCalendar.js
@@ -3,9 +3,7 @@ const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const TIME_ZONE = "CST";
-const serverAuthCode =
-  "4/tgFh5Ve4IheQ2yRY8uORml7z3esvwE1b9dbwUwV9ryAlWoT2vQlNX64lz9bI2GwKXyQBC6oqXlML1KYZqYwWTwI";
+const TIME_ZONE = "America/Chicago";
 
 try {
   admin.initializeApp(functions.config().firebase);
@@ -14,34 +12,44 @@ try {
 }
 
 const db = admin.firestore();
+let trainerId = null;
 
 module.exports = functions.firestore
   .document("events/{eventId}")
-  .onCreate(snap => {
+  .onCreate(async snap => {
     const newEvent = snap.data();
+    trainerId = newEvent.trainerId;
 
-    // Trying to just get the first session we find until we get this thing working.
-    const sessionQuery = db.collection("sessions").limit(1);
+    const trainer = await db
+      .collection("users")
+      .doc(newEvent.trainerId)
+      .get()
+      .then(doc => {
+        console.log(doc.data());
+        return doc.data();
+      });
 
-    const session = sessionQuery.get();
-
-    console.log(session.name);
-    console.log(session.description);
-    console.log(newEvent.startTime);
-    console.log(newEvent.endTime);
+    const session = await db
+      .collection("sessions")
+      .doc(newEvent.sessionId)
+      .get()
+      .then(doc => {
+        console.log(doc.data());
+        return doc.data();
+      });
 
     const eventData = {
-      eventName: "Test name",
-      description: "Test description",
-      startTime: newEvent.startTime,
-      endTime: newEvent.endTime
+      eventName: session.name,
+      description: session.description,
+      startTime: new Date(newEvent.startTime._seconds * 1000).toISOString(),
+      endTime: new Date(newEvent.endTime._seconds * 1000).toISOString()
     };
 
-    createEvent(eventData);
+    createEvent(eventData, trainer.tokens);
   });
 
-async function createEvent(eventData) {
-  const oAuth2Client = await getOauthClient(serverAuthCode);
+async function createEvent(eventData, tokens) {
+  const oAuth2Client = await getOauthClient(tokens);
 
   calendar.events.insert(
     {
@@ -60,33 +68,54 @@ async function createEvent(eventData) {
         }
       }
     },
-    (err, event) => {
+    err => {
       if (err) {
         console.log(
           "There was an error contacting the Calendar service: " + err
         );
         return;
       }
-      console.log("Event created: %s", event.htmlLink);
+      console.log(
+        `Successfully created event for trainer: ${trainerId} \n ${JSON.stringify(
+          eventData
+        )}`
+      );
     }
   );
 }
 
-function getOauthClient(serverAuthCode) {
+async function getOauthClient(userTokens) {
+  const { accessToken, refreshToken, serverAuthCode } = userTokens;
   const clientID =
-    "313017879174-smoahcsderaaprplpvg8a6ka3gtacg54.apps.googleusercontent.com";
-  const clientSecret = "SrLxagKZz0AHgKPsUDyb-XRC";
-  const oauth = new OAuth2(clientID, clientSecret);
-  let accessToken = null;
+    "313017879174-c59296h7qtlfjhq0dcq5csk5d1a55hib.apps.googleusercontent.com";
+  const clientSecret = "CkV0junuAhslcBqhh8V39p8Y";
+  const redirectUrl = "https://lab-days.firebaseapp.com/__/auth/handler";
+  const oauth = new OAuth2(clientID, clientSecret, redirectUrl);
 
-  try {
-    accessToken = oauth.getToken(serverAuthCode);
-  } catch (e) {
-    console.log(`Error getting access token: ${e}`);
+  if (refreshToken && accessToken) {
+    console.log("refreshing tokens");
+    await oauth.setCredentials({
+      access_token: accessToken,
+      refresh_token: refreshToken
+    });
+  } else {
+    try {
+      const { tokens } = await oauth.getToken(serverAuthCode);
+      console.log(tokens);
+
+      const trainer = db.collection("users").doc(trainerId);
+      trainer.update({
+        tokens: {
+          accessToken: tokens.access_token,
+          refreshToken: tokens.refresh_token
+        }
+      });
+
+      await oauth.setCredentials(tokens);
+    } catch (e) {
+      console.log(`Error getting access token: ${e}`);
+    }
   }
 
-  console.log("oath getToken response:");
-  console.log(accessToken);
-  oauth.setCredentials({ access_token: accessToken });
   return oauth;
 }
